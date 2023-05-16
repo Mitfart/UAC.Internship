79188a18845e02ffc751ac6520cdea3d36b678b1
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-05-16 14:38:41 +0400

    Update 1_FizzBuzz.js

--- 1_FizzBuzz.js
+++ 1_FizzBuzz.js
@@ -6,7 +6,8 @@
 */
 
 for (let i = 0; i < 100; i++) {
-   if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
-   else if (i % 3 === 0) console.log("Fizz");
-   else if (i % 5 === 0) console.log("Buzz");
+   let msg = '';
+   if (i % 3 === 0) msg += "Fizz"
+   if (i % 5 === 0) msg += "Buzz"
+   if (msg) console.log(msg);
 }
