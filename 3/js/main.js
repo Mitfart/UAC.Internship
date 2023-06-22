f2d9a4364f10fcd34f28c35267e138536ab19265
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-06-22 20:54:14 +0400

    _

--- 3/js/main.js
+++ 3/js/main.js
@@ -8,6 +8,28 @@ $(function () {
    });
 
    $('#to-top_btn').click(function () {
-      $('body, html').animate({ scrollTop: 0 }, 0)
+      $('body, html').animate({scrollTop: 0}, 0)
    });
+
+   /*
+   https://codepen.io/dimsemenov/pen/kVbqdN
+   ====== Zoom effect ======
+   */
+   $('.thumbnails').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+         enabled: true
+      },
+      removalDelay: 500, //delay removal by X to allow out-animation
+      callbacks: {
+         beforeOpen: function () {
+            // hack: adds mfp-anim class to markup
+            this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+            this.st.mainClass = this.st.el.attr('data-effect');
+         }
+      },
+      closeOnContentClick: true,
+      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+   })
 });
